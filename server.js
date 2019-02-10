700ab055dc04fb71f83e9f9631ee141278c47308
Kevin Conway <kpconway519>
2019-02-10 17:55:31 -0500

    pushing working version

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var axios = require('axios')
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var request = require("request");
-var rp = require("request-promise");
+// var request = require("request");
+// var rp = require("request-promise");
 var cheerio = require("cheerio");
 
 // Require all models
@@ -57,21 +58,24 @@ mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true }
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.nytimes.com/").then(function(response) {
+  axios.get("https://www.nytimes.com/section/arts").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("a.story-link").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
+      result.title =
+      $(element)
+        .children("div.story-meta")
+        .children("h3")
         .text();
-      result.link = $(this)
-        .children("a")
+
+      result.link =
+      $(element)
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
